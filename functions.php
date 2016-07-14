4d77f2dd5fd3855e0b956bf65f1d81efc5c2f2de
AndrewMowe <andrew@team-eight.com>
2016-07-14 16:23:27 -0400

    add js

--- functions.php
+++ functions.php
@@ -3,4 +3,5 @@
  * Child-Theme functions and definitions
  */
 
-?>
+wp_enqueue_script( 'jquery' );
+wp_enqueue_script( 'custom-js', 'main.js', array( 'jquery' ) );
---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('.infographic').on('hover', function(){
+
+    	alert('peepee');
+      //$('img').attr('src', 'http://images2.wikia.nocookie.net/__cb20060607095225/uncyclopedia/images/5/59/Counter.gif');
+
+    }, function() {
+
+    	alert('poop');
+
+    });
+
+});
