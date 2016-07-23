b4968f98244c0df752b90fb761afc3f4a5b65fdc
spencer8 <specer@team-eight.com>
2016-07-23 17:11:06 -0400

    header buttons

--- functions.php
+++ functions.php
@@ -5,3 +5,10 @@
 
 wp_enqueue_script( 'jquery' );
 wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/main.js', array( 'jquery' ) );
+
+function dlva_child_theme_setup() {
+
+	register_nav_menu( 'head_butt_rt', __( 'Header right buttons', 'children_theme' ) );
+
+}
+add_action( 'after_setup_theme', 'dlva_child_theme_setup' );
